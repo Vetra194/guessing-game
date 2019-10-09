19bf7af6c0ff3fc44a82c43f10b68771bba700a7
Vetra194 <m_sol88@mail.ru>
2019-10-09 23:51:25 +0400

    feat: add solution for guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.solution = Math.ceil((this.min + this.max)/2);
+        return this.solution;
     }
 
     lower() {
-
+        this.max = this.solution; 
     }
 
     greater() {
-
+        this.min = this.solution;
     }
 }
 
